195a6d41ad8f3eea0c9ac39e8f28b83f7e52e4dc
Lucas13Marques <lucasgabrielmr982@gmail.com>
2025-08-06 22:53:07 -0300

    criacao de lista e armazenamento de nomes

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() { 
+    let digitAmigo  = document.querySelector('input').value
+
+amigos.push(digitAmigo)
+
+console.log(amigos)
+}
