04285039b4e4463fc4d60c1bad6ca50dc818fee8
Lucas13Marques <lucasgabrielmr982@gmail.com>
2025-08-06 22:53:48 -0300

    criacao de alert se valor for vazio

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
 
 function adicionarAmigo() { 
+
+    if (document.querySelector('input').value == ''){
+        alert('Digite um valor válido')
+
+    }else{
     let digitAmigo  = document.querySelector('input').value
 
 amigos.push(digitAmigo)
 
 console.log(amigos)
-}
+    }}
