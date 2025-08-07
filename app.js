ad427ee774d35154017e4806a7a5f34642ffd4a1
Lucas13Marques <lucasgabrielmr982@gmail.com>
2025-08-07 00:26:05 -0300

    comentarios explicativos

--- app.js
+++ app.js
@@ -1,38 +1,60 @@
 let amigos = [];
 
-
+// Função responsável por exibir os amigos na lista <ul>
 function exibirAmigos(){ 
+
+//Busca o elementro a partir do id
     let lista = document.getElementById('listaAmigos');
+
+//Deixa em branco qualquer texto interno no elemento
         lista.innerHTML = '';
 
+//Cria loop com for, cria a variavel, adiciona condicional e adiciona valor a variavel 1 para finalizar o loop 
     for( let i = 0;i < amigos.length; i++ ){ 
+
+// Cria um novo elemento <li> 
         let item = document.createElement('li');
+
+// Define o texto do <li> como o nome do amigo atual
         item.textContent = amigos[i];
+
+// Adiciona o <li> ao <ul>
         lista.appendChild(item);
        
     }}
 
+    
+// Função responsável por adicionar um novo nome no array e atualizar a exibição
 function adicionarAmigo() { 
 
+// Verifica se o campo está vazio
     if (document.querySelector('input').value == ''){
+       
+// Alerta o usuário se nenhum nome foi inserido
         alert('Por favor, insira um nome.')
 
+// Captura o valor digitado no campo <input>
     }else{
     let digitAmigo  = document.querySelector('input').value;
 
 amigos.push(digitAmigo);
 
+// Exibe o array atualizado no console (útil para depuração)
 console.log(amigos);
+
+ // Atualiza a lista exibida na tela
 exibirAmigos() ;   
 
     }
+    
+ // Limpa o campo de entrada após o envio
 limparcampos();}
 
 
-    
+// Função para limpar o campo de entrada do usuário
 function limparcampos() { 
-    chute = document.querySelector('input');
-    chute.value = '';
+    digitAmigo = document.querySelector('input');
+    digitAmigo.value = '';
 }
 
 
